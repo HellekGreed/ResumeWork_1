4e7d71784e2f6a5091246f049a617dab139df2d6
Hellek-Greed <nimnarr@gmail.com>
2022-10-10 14:20:02 +0300

    Metod GetArray added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] GetArray()
+{
+        Console.Write("Введите желаемую размерность массива(Это должно быть целое число): ");
+        int size = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+        string[] array = new string[size];
+
+        Console.WriteLine("Введите желаемые строки(после каждой строки нажмите Enter).");
+        for(int i = 0; i < size; i++)
+        {
+                str = Console.ReadLine();
+                array[i] = str;
+        }
+
+        return array;
+}
+
